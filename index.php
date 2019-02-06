62be3bc62a72796ac213e312113021bdfe3fd6fc
techdroweb <45241872+techdroweb@users.noreply.github.com>
2019-02-06 13:13:59 +0600

    learing some html staff

--- index.php
+++ index.php
@@ -6,8 +6,65 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <style>
+        table, th, td {
+            border: 0px solid black;
+            border-collapse: collapse;
+        }
+
+        td {
+            padding: 20px;
+        }
+        body {
+            background-color: antiquewhite;
+        }
+    </style>
 </head>
 <body>
+
+<table cellspacing="20">
+    <tr>
+        <td><img src="images/Angela%20yu.jpg" alt="Angela Yu" style="height: 200px; width: 200px"></td>
+        <td>
+            <h1>Angela Yu</h1>
+            <em>Founder and TO of The App Brewery.</em>
+            <p>I am an iOS and Web Developer. I love coffee and brew my own beers.</p>
+        </td>
+    </tr>
+</table>
+
+
+<hr>
+
+<h3>Books and Teaching</h3>
+<ul>
+    <li>The Complete iOS App Development Bootcamp</li>
+    <li>The Complete Web Development Bootcamp</li>
+</ul>
+<hr>
+
+<h3>Work Experience</h3>
+<table>
+    <tr>
+        <th>Dates</th>
+        <th>Work</th>
+    </tr>
+
+    <tr>
+        <td>2009-2010</td>
+        <td>Researcher at The Institute of Cognitive Neuroscience</td>
+    </tr>
+    <tr>
+        <td>2010-2013</td>
+        <td>Lead Developer at Tempo App</td>
+    </tr>
+    <tr>
+        <td>2013-2015</td>
+        <td>Clinical Doctor ata the National Health Service</td>
+    </tr>
+
+
+</table>
 
 <table>
     <h3>Skills</h3>
